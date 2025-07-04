f2066a8136a031ea3eeacc6e36f5d1c3bbea3032
tommyiversonj <tommyiversonj@gmail.com>
2025-07-04 00:17:21 +0000

    Update

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
