7ab28263cda9d559ac42ca08ad18ce995ee20560
tommyiversonj <tommyiversonj@gmail.com>
2025-07-04 00:02:26 +0000

    Update

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,17 @@
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+-- task_4.sql
+
+-- Select the database to use
+USE alx_book_store;
+
+-- Print the full description of the 'books' table without using DESCRIBE or EXPLAIN
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
