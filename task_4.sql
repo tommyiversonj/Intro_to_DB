d59bc19aad603a2ef2ac204da4e4b0a17952137b
tommyiversonj <tommyiversonj@gmail.com>
2025-07-04 00:11:49 +0000

    Update

--- task_4.sql
+++ task_4.sql
@@ -1,17 +1,4 @@
--- task_4.sql
-
--- Select the database to use
-USE alx_book_store;
-
--- Print the full description of the 'books' table without using DESCRIBE or EXPLAIN
-SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books'
+  AND TABLE_SCHEMA = 'alx_book_store';
