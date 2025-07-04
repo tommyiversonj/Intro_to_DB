605956658b62db37a0e8fbe95dd30ac4f3d4df78
tommyiversonj <tommyiversonj@gmail.com>
2025-07-04 00:14:17 +0000

    Update

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,11 @@
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books'
-  AND TABLE_SCHEMA = 'alx_book_store';
+INSERT INTO Customers (
+    customer_id,
+    customer_name,
+    email,
+    address
+) VALUES (
+    1,
+    'Cole Baidoo',
+    'cbaidoo@sandtech.com',
+    '123 Happiness Ave.'
+);
